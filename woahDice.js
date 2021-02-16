d4341f6a49317819eccaff88c4fd3f8f2a1eadc2
Kurt Snuggly <unsnugHero@gmail.com>
2021-02-15 20:41:32 -0500

    Some preliminary code

--- woahDice.js
+++ woahDice.js
@@ -3,14 +3,30 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 const token = process.env.TOKEN;
 
+// command we are looking for
+const commandPrefix = '!roll';
+const yeahPrefiex = 'yeah';
+
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
 client.on('message', (msg) => {
-  if (msg.content === 'ping') {
-    msg.reply('Pong!');
+  const { content } = msg;
+
+  if (content.startsWith(commandPrefix)) {
+    msg.reply('Do you wanna roll with me? Say yeah.');
+    charliXCX = true;
+
+    return;
   }
+
+  if (content.startsWith(yeahPrefiex)) {
+    msg.reply('Yeahiyeaiyea');
+    charliXCX = false;
+  }
+
+  charliXCX = false;
 });
 
 client.login(token);
